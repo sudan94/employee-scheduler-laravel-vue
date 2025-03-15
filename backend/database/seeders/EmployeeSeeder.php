2b1a10cf5966674f52e505fe6f1b827f1692d397
sudan94 <sudanupadhaya@gmail.com>
2025-03-15 13:50:17 +0100

    Added employee seeder

---
+++ backend/database/seeders/EmployeeSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Employee;
+use Faker\Factory as Faker;
+
+class EmployeeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $faker = Faker::create();
+
+        for ($i = 0; $i < 1000; $i++) {
+            Employee::create([
+                'name' => $faker->name,
+                'email' => $faker->unique()->safeEmail,
+                'phone' => $faker->phoneNumber,
+                'address' => $faker->address,
+                'job_title' => $faker->jobTitle,
+            ]);
+        }
+    }
+}
