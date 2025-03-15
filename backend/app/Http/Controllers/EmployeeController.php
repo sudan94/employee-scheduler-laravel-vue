f6ca03e2f6cc11b95afdfc796a2616d89bef122c
sudan94 <sudanupadhaya@gmail.com>
2025-03-15 13:19:09 +0100

    Added CRUD operations for employees

---
+++ backend/app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\EmployeeResource;
+use App\Models\Employee;
+use Illuminate\Http\Request;
+
+class EmployeeController extends Controller
+{
+     public function index(){
+        return EmployeeResource::collection(Employee::paginate(50));
+     }
+
+     public function store(Request $request)
+     {
+        $employee = Employee::create($request->all());
+        return new EmployeeResource($employee);
+     }
+
+     public function update(Request $request, Employee $employee)
+     {
+        $employee->update($request->all());
+        return new EmployeeResource($employee);
+     }
+
+     public function destroy(Employee $employee)
+     {
+        $employee->delete();
+        return response()->json(['message' => 'Employee deleted'],204);
+     }
+}
